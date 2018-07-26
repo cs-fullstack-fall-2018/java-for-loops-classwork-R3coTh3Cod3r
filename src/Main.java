1645a46924a9de6632654e5e7cef417cbdd4f602
Mareco Nabors <naborsmarico99@gmail.com>
2018-07-25 23:10:02 -0500

    fixing problem

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  //  public static void main(String[] args) {
+    //    int lastNumber = 100;
+      //  System.out.println("Starting Count to " + lastNumber);
+        //countTheNumbers(lastNumber);
+   // }
+
+    //public static void countTheNumbers(int lastNumber) {
+
+      //  for (int countTheNumber = 0; countTheNumber<lastNumber;  countTheNumber++)
+        //{
+   // System.out.println(countTheNumber);
+     //   }
+
+
+public static void main(String[] args){
+    String[] names = {"Bob","Kenn","Kevin","Erin"};
+    ArrayList<String> studentList = new ArrayList<String>();
+    studentList.addAll(Arrays.asList(names));
+    System.out.println(studentList);
+
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
